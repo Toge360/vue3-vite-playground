ac6b63585ab28403bad3102f2a505abfb26348c8
Tobias Gerlach <tobias@gerlach360.de>
2021-10-12 21:16:32 +0200

    setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,15 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
+// importing components
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 
-let routes = [
-  {
-    path: '/',
-    name: 'home',
-    icon: 'mdi-monitor-dashboard',
-    component: () => import('../views/Home.vue'),
-    meta: {
-      requiresAuth: true
-    },
-  },
-
-  {
-    path: '/404',
-    name: '404',
-    icon: '',
-    inNav: false,
-    component: () => import('../views/404.vue'),
-    meta: {
-      requiresAuth: true
-    }
-  }
-
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: routes
-})
-
-
-/* Navigation Guard 
-Prevents access to a blocked route
-https://router.vuejs.org/guide/advanced/navigation-guards.html*/
-
-router.beforeEach((to, from, next) => {
-  //
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About },
+  ]
 })
 
 export default router

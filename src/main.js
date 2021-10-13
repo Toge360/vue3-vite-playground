e20b5d552b05bc9b4c6b7cc3a3ba3fe9f71d8662
Tobias Gerlach <tobias@gerlach360.de>
2021-10-13 20:55:37 +0200

    import axios

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
 import store from './store/index.js'
-
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 createApp(App)
 .use(router)
 .use(store)
+.use(VueAxios, axios)
 .mount('#app')
 

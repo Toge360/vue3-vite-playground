78555cdbeb059be4bec06609eb84dbf0996105cb
Tobias Gerlach <tobias@gerlach360.de>
2021-10-15 21:15:01 +0200

    init

---
+++ src/components/DemoComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <h2>{{component.title}} with prop: "{{demoProp}}"</h2>
+</template>
+
+<script>
+import { ref, reactive} from 'vue' // vue 'functionality'
+export default {
+
+  name: 'DemoComponent',
+
+  props: {
+    demoProp: String
+  },
+
+  setup() {
+
+    const component = reactive({ title: 'Demo Component' })
+    const loading = ref(false)
+
+    return {
+      component,
+      loading
+    }
+  
+  }
+
+}
+</script>
+
+<style>
+
+</style>

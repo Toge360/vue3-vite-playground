a92036cbe7bc3c006973f6ceb07a8b89de222443
Tobias Gerlach <tobias@gerlach360.de>
2021-10-15 21:15:23 +0200

    using a demo component with a prop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,18 @@
 
 <template>
-  <h1>{{book.title}}</h1><br/>
+  <h1>{{page.title}}</h1><br/>
 
   <h2>VUEX Store</h2>
   <button @click="this.$store.commit('increment')" v-text="`Ich wurde ${this.$store.state.count} Mal geklickt`"></button>
 
+  <DemoComponent demoProp="I am a prop"></DemoComponent>
+
   <h2>List-Rendering</h2>
 
-  <div v-if="loading">Fetching</div>
+  <div v-if="loading">
+    Fetching data ...
+  </div>
+
   <div v-else style="text-align:left;">
     <ul id="array-rendering">
       <li v-for="item in data.data" :key="item.id">
@@ -21,13 +26,16 @@
 <script>
 import { ref, reactive, onMounted} from 'vue' // vue 'functionality'
 import { get } from '../api/api'
+import DemoComponent from "../components/DemoComponent.vue"
 export default {
 
+  components: {
+    DemoComponent
+  },
 
   setup() {
 
-    const readersNumber = ref(0)
-    const book = reactive({ title: 'Vue 3 Guide' })
+    const page = reactive({ title: 'Vue 3 Composition API' })
     const data = ref([])
     const loading = ref(false)
 
@@ -37,11 +45,10 @@ export default {
       loading.value = false
     }
 
-    onMounted(getData) // created not needed in composition api
+    onMounted(getData) // 'created' not needed in composition api
 
     return {
-      readersNumber,
-      book,
+      page,
       data,
       loading
     }

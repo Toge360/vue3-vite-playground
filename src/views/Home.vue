daad5d84828ac389a3247ceb5a35f044f09e7db0
Tobias Gerlach <tobias@gerlach360.de>
2021-10-15 17:41:40 +0200

    Playing around
    import own api class, make an api call, loading ref, onMounted hook

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,49 @@
 
 <template>
-  {{book.title}}<br/>
-  <button @click="this.$store.commit('increment')" v-text="`VUEX-Store: ${this.$store.state.count}`"></button>
+  <h1>{{book.title}}</h1><br/>
+
+  <h2>VUEX Store</h2>
+  <button @click="this.$store.commit('increment')" v-text="`Ich wurde ${this.$store.state.count} Mal geklickt`"></button>
+
+  <h2>List-Rendering</h2>
+
+  <div v-if="loading">Fetching</div>
+  <div v-else style="text-align:left;">
+    <ul id="array-rendering">
+      <li v-for="item in data.data" :key="item.id">
+        {{ item.title }}
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script>
-import { ref, reactive } from 'vue' // reactive 'feature'
-
+import { ref, reactive, onMounted} from 'vue' // vue 'functionality'
+import { get } from '../api/api'
 export default {
-  
+
+
   setup() {
 
     const readersNumber = ref(0)
     const book = reactive({ title: 'Vue 3 Guide' })
+    const data = ref([])
+    const loading = ref(false)
+
+    const getData = async () => {
+      loading.value = true
+      data.value = await new get().request('https://jsonplaceholder.typicode.com/posts')
+      loading.value = false
+    }
+
+    onMounted(getData) // created not needed in composition api
 
     return {
       readersNumber,
-      book
+      book,
+      data,
+      loading
     }
   
   }
@@ -24,15 +51,3 @@ export default {
 }
 
 </script>
-
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
